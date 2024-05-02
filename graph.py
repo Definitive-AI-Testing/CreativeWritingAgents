831b08c3625d9050b5ef6574c65154f620e54f6d
Jeremiah Jeschke <mail@officeautomata.com>
2024-05-02 18:20:08 -0500

    Update graph.py

--- graph.py
+++ graph.py
@@ -12,6 +12,7 @@ from StreamlitTools import StreamlitInput
 __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+import sqlite3
 
 load_dotenv()
 with st.sidebar:
