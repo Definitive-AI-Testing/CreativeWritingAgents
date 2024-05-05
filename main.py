5271a9ebca11ff30679676058badf21ae69336be
Jeremiah Jeschke <mail@officeautomata.com>
2024-05-05 14:55:40 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,14 @@ from pathlib import Path
 import streamlit as st
 from st_pages import Page, show_pages, add_page_title
 
-with st.echo("below"):
-    from st_pages import Page, add_page_title, show_pages
+#with st.echo("below"):
+"## Declaring the pages in your app:"
 
-    "## Declaring the pages in your app:"
+show_pages(
+    [
+        Page("main.py", "Home", "🏠"),
+        Page("graph.py", "Content Creation Team"),
+    ]
+)
 
-    show_pages(
-        [
-            Page("main.py", "Home", "🏠"),
-            Page("graph.py", "Content Creation Team"),
-        ]
-    )
-
-    add_page_title()  # Optional method to add title and icon to current page
+add_page_title()  # Optional method to add title and icon to current page
