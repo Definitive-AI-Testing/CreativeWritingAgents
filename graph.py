7b39ba163687cb4b3a8437bf106f158d459180da
Jeremiah Jeschke <mail@officeautomata.com>
2024-05-14 15:55:44 -0500

    Update graph.py

--- graph.py
+++ graph.py
@@ -111,9 +111,12 @@ Key ideas to include:
 ●	Allows you to take advantage of mixing in low cost alternatives with your top self RPA tools. 
 """
         #user_input = st.text_input("Enter topic, key ideas, products, potential key phrases, and example articles:")
-        input1 = streamlit_tool._run("Enter topic, key ideas, products, potential key phrases, and example articles:")
+        message = st.chat_message("assistant")
+        message.write("Enter topic, key ideas, products, potential key phrases, and example articles")
+        initial = st.chat_input("Enter Text", key="user_input1")
+
         widget_update_func = st.empty().code
-        input_data = {"input": input1}
+        input_data = {"input": initial}
 
         for s in app.stream(input_data):
             print(s)
